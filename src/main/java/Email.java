2634ac5e68d5f75f02b85f48d155b659c3c46acb
Gaster <abdelrahman.5alil@outlook.com>
2022-05-01 02:31:10 +0200

    finishing the Email class

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -12,8 +12,6 @@ public class Email{
     String host = "localhost";
  
     Properties properties = System.getProperties();
-
-    
     
     Email(String to, String from){
     	
@@ -22,11 +20,29 @@ public class Email{
     	this.to = to;
     	
     	properties.setProperty("mail.smtp.host", host);
-    	
-    	Session session = Session.getDefaultInstance(properties);
-    	
+    	  
     }
     
-    
+    void sendEmail(String SubjectString, String MessageString)
+    {
+    	Session session = Session.getDefaultInstance(properties);
+    	
+    	try {
+    	MimeMessage message = new MimeMessage(session);
+    	
+    	message.setFrom(new InternetAddress(from));
+    	
+    	message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+    	
+    	message.setSubject(SubjectString);
+    	
+    	message.setText(MessageString);
+    	
+    	Transport.send(message);
+    	   	
+    	}catch(MessagingException mex) {
+    		mex.printStackTrace();
+    	}
+    }
 
 }
