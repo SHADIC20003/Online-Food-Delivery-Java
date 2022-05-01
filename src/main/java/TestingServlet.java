6d11b25477eb7c26445c7330765ff6274a32d66e
Gaster <abdelrahman.5alil@outlook.com>
2022-05-01 03:22:30 +0200

    making sure a few more libraries do exist

--- src/main/java/TestingServlet.java
+++ src/main/java/TestingServlet.java
@@ -11,6 +11,16 @@ import javax.mail.internet.*;
 import javax.activation.*;
 import java.io.PrintWriter;
 import jakarta.servlet.RequestDispatcher;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class TestingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
